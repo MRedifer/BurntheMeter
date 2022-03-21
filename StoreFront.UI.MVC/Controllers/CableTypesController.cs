71e9ee56a4ef8acfe60748a0d488b18015236dec
Michael Redifer <mikeredifer@yahoo.com>
2022-03-21 15:47:26 -0500

    Scaffolded controllers and views

---
+++ StoreFront.UI.MVC/Controllers/CableTypesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using StoreFront.DATA.EF;
+
+namespace StoreFront.UI.MVC.Controllers
+{
+    public class CableTypesController : Controller
+    {
+        private BurntheMeterEntities db = new BurntheMeterEntities();
+
+        // GET: CableTypes
+        public ActionResult Index()
+        {
+            return View(db.CableTypes.ToList());
+        }
+
+        // GET: CableTypes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CableType cableType = db.CableTypes.Find(id);
+            if (cableType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cableType);
+        }
+
+        // GET: CableTypes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CableTypes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CableTypeID,CableTypeName,Description")] CableType cableType)
+        {
+            if (ModelState.IsValid)
+            {
+                db.CableTypes.Add(cableType);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(cableType);
+        }
+
+        // GET: CableTypes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CableType cableType = db.CableTypes.Find(id);
+            if (cableType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cableType);
+        }
+
+        // POST: CableTypes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CableTypeID,CableTypeName,Description")] CableType cableType)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cableType).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(cableType);
+        }
+
+        // GET: CableTypes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CableType cableType = db.CableTypes.Find(id);
+            if (cableType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cableType);
+        }
+
+        // POST: CableTypes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            CableType cableType = db.CableTypes.Find(id);
+            db.CableTypes.Remove(cableType);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ StoreFront.UI.MVC/Controllers/StockStatusController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using StoreFront.DATA.EF;
+
+namespace StoreFront.UI.MVC.Controllers
+{
+    public class StockStatusController : Controller
+    {
+        private BurntheMeterEntities db = new BurntheMeterEntities();
+
+        // GET: StockStatus
+        public ActionResult Index()
+        {
+            return View(db.StockStatus.ToList());
+        }
+
+        // GET: StockStatus/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StockStatu stockStatu = db.StockStatus.Find(id);
+            if (stockStatu == null)
+            {
+                return HttpNotFound();
+            }
+            return View(stockStatu);
+        }
+
+        // GET: StockStatus/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: StockStatus/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "StockStatusID,StockStatus,Notes")] StockStatu stockStatu)
+        {
+            if (ModelState.IsValid)
+            {
+                db.StockStatus.Add(stockStatu);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(stockStatu);
+        }
+
+        // GET: StockStatus/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StockStatu stockStatu = db.StockStatus.Find(id);
+            if (stockStatu == null)
+            {
+                return HttpNotFound();
+            }
+            return View(stockStatu);
+        }
+
+        // POST: StockStatus/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "StockStatusID,StockStatus,Notes")] StockStatu stockStatu)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(stockStatu).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(stockStatu);
+        }
+
+        // GET: StockStatus/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StockStatu stockStatu = db.StockStatus.Find(id);
+            if (stockStatu == null)
+            {
+                return HttpNotFound();
+            }
+            return View(stockStatu);
+        }
+
+        // POST: StockStatus/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            StockStatu stockStatu = db.StockStatus.Find(id);
+            db.StockStatus.Remove(stockStatu);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
